148a66ce518a13a352fb764e61b6f4ca5c103fef
zhouzhezz <zorichzorich@gmail.com>
2018-11-05 13:13:36 -0500

    clean the code

--- user_code.ml
+++ user_code.ml
@@ -70,17 +70,6 @@ let printvec_dense vec =
     match e with
     | (_, value) -> print_int value; print_string " ");
     print_string "\n";;
-(*
-let vec1 = Library.DenseMatrix.to_vector [1;2;0;4] in
-let vec2 = Library.DenseMatrix.to_vector [1;2;0;4] in
-print_int (dot_dense_dense vec1 vec2); print_string "\n";;
-let vec1 = Library.SparseMatrix.to_vector [1;2;0;4] in
-let vec2 = Library.SparseMatrix.to_vector [1;2;0;4] in
-print_int (Library.SparseMatrix.look vec1 0); print_string " ";
-print_int (Library.SparseMatrix.look vec1 1); print_string " ";
-print_int (Library.SparseMatrix.look vec1 2); print_string " ";
-print_int (Library.SparseMatrix.look vec1 3); print_string " ";
-print_int (dot_sparse_sparse vec1 vec2); print_string "\n";; *)
 
 let printmat_dense mat =
     Library.DenseMatrix.foldcoll mat ()
@@ -93,16 +82,6 @@ let printmat_sparse mat =
     (fun _ e ->
     match e with
     | (i, value) -> print_int i; print_string ": "; printvec_sparse value);;
-
-(* let rec printvec_list vec =
-    List.map (fun e ->
-        print_int e; print_string " ") vec;
-    print_string "\n";;
-
-let rec printmat_list mat =
-    match mat with
-    | [] -> ()
-    | h :: t -> printvec_list h; printmat_list t;; *)
 
 let multiply_dense_dense_dense mat1 mat2 =
         Library.DenseMatrix.to_matrix (Library.DenseMatrix.foldcoll mat1 []
@@ -144,28 +123,6 @@ let multiply_sparse_sparse_sparse mat1 mat2 =
                     result_row@[dot_result]) in
             result_mat@[new_row]));;
 
-(* let show_dense_dense mat1 mat2 =
-        Library.DenseMatrix.foldcoll mat1 ()
-        (fun _ (i, row_of_mat1) ->
-            Library.DenseMatrix.foldrowl mat2 ()
-            (fun _ (j, col_of_mat2) ->
-                print_int i; print_string "[i]: ";
-                printvec_dense row_of_mat1;
-                print_int j; print_string "[j]: ";
-                printvec_dense col_of_mat2;
-                ));;
-
-let show_sparse_sparse mat1 mat2 =
-        Library.SparseMatrix.foldcoll mat1 ()
-        (fun _ (i, row_of_mat1) ->
-            Library.SparseMatrix.foldrowl mat2 ()
-            (fun _ (j, col_of_mat2) ->
-                print_int i; print_string "[i]: ";
-                printvec_sparse row_of_mat1;
-                print_int j; print_string "[j]: ";
-                printvec_sparse col_of_mat2;
-                ));; *)
-
 (* main *)
 let mat2 =
     [[1;0;3;4];
@@ -199,32 +156,3 @@ match (if_sparse_mat1, if_sparse_mat2) with
     let mat2'= Library.DenseMatrix.to_matrix mat2 in
     print_string "DenseMatrix x DenseMatrix\n";
     printmat_dense (multiply_dense_dense_dense mat1' mat2');;
-
-
-(* let mat1 = Library.DenseMatrix.to_matrix
-    [[1;0;3;4];
-     [1;2;0;4];
-     [1;0;0;0]] in
-let mat2 = Library.DenseMatrix.to_matrix
-    [[1;0;3];
-     [1;2;3];
-     [0;2;0];
-     [0;2;3]] in
-printmat_dense (multiply_dense_dense_dense mat1 mat2);;
-
-let mat1 = Library.SparseMatrix.to_matrix
-    [[1;0;3;4];
-     [1;2;0;4];
-     [1;0;0;0]] in
-let mat2 = Library.SparseMatrix.to_matrix
-    [[1;0;3];
-     [1;2;3];
-     [0;2;0];
-     [0;2;3]] in
-printmat_sparse (multiply_sparse_sparse_sparse mat1 mat2);;
-(* show_sparse_sparse mat1 mat2;; *)
-
-Printf.printf "%B\n" (if_sparse [[1;0;3];
-     [1;2;3];
-     [0;2;0];
-     [0;2;3]]);; *)
